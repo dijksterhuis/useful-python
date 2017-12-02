e021458ef1a7f90915d1762ca2691778ca8a56f5
dijksterhuis <mrobeson@dundee.ac.uk>
2017-12-02 19:29:14 +0000

    updates to SQLStatement and created UnitTester

--- SQLStatement.py
+++ SQLStatement.py
@@ -1,121 +1,228 @@
-class SQLStatement:
+from UnitTester import UnitTester
+
+def demo():
+	print("a = SQLBuilder()")
+	a = SQLBuilder()
+	print(a.query_get())
+	
+	print("a.sql( (('INSERT INTO','table'),) )")
+	a.sql( (('INSERT INTO','table'),) )
+	print(a.query_get())
+	
+	print("a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )")
+	a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )
+	print(a.query_get())
+	
+	print("	a.forget('SELECT (*)')")
+	a.forget('SELECT (*)')
+	print(a.query_get())
+	
+	print("a.sql( [['FROM','table_name']] )")
+	a.sql( [['FROM','table_name']] )
+	print(a.query_get())
+	
+	print("a.sql( {'WHERE':(('everything','nothing'),('site','example.com'))} )")
+	a.sql( {'WHERE':(('everything','nothing'),('site','example.com'))} )
+	print(a.query_pop())
+	
+	print("a.sql( (('INSERT INTO','table'),) )")
+	a.sql( (('INSERT INTO','table'),) )
+	print(a.query_get())
+	
+	print("a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )")
+	a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )
+	print(a.query_pop())
+	
+	print("print(a.query_pop())")
+	print(a.query_get())
+	
+def unit_test():
+	a, test_results = SQLBuilder(), UnitTester()
+	
+	# Test 1
+	test_results.add_result( a.query_get() == '')
+	
+	# Test 2
+	a.sql( (('INSERT INTO','table'),) )
+	test_results.add_result(a.query_get() == 'INSERT INTO table ')
+	
+	# Test 3
+	# --- Can fail due to dictionary ordering...!
+	a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )
+	test_results.add_result(a.query_get() == 'INSERT INTO table (everything, site) VALUES (nothing, example.com) ' or a.query_get() == 'INSERT INTO table (site, everything) VALUES (example.com, nothing) ')
+	
+	# Test 4
+	a.forget('SELECT (*)')
+	test_results.add_result(a.query_get() == 'SELECT (*) ')
+	
+	# Test 5
+	a.sql( [['FROM','table_name']] )
+	test_results.add_result(a.query_get() == 'SELECT (*) FROM table_name ')
+	
+	# Test 6
+	a.sql( {'WHERE':(('everything','nothing'),('site','example.com'))} )
+	test_results.add_result(a.query_pop() == 'SELECT (*) FROM table_name WHERE everything = nothing, site = example.com ')
+	
+	# Test 7
+	a.sql( (('INSERT INTO','table'),) )
+	test_results.add_result(a.query_get() == 'INSERT INTO table ')
+	
+	# Test 8
+	# --- Can fail due to dictionary ordering...!
+	a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )
+	test_results.add_result(a.query_get() == 'INSERT INTO table (everything, site) VALUES (nothing, example.com) ' or a.query_get() == 'INSERT INTO table (site, everything) VALUES (example.com, nothing) ')
+	a.query_pop()
+	
+	# Test 9
+	test_results.add_result(a.query_get() == '')
+	test_results.output()
+	
+class SQLBuilder:
 	"""
-	Object to handle bulding SQL queries from keyword argument/text inputs
+	==================================================================
+	Object to buld text SQL queries from packed argument/text inputs
+	==================================================================
+	INFO
 	---------------------------------------
-	Usage:
-	>>> foo = SQLStatement()
-	>>> foo.query()
+	SQLBuilder('starting string') is used to initialise the query string.
+	- Takes either:
+		- packed argument parameters
+			- dictionary of commands e.g.:
+			 	- {'SELECT' : '(*)' , 'FROM' : 'table' }
+			- nested tuples:
+				- (('INSERT INTO','table'),)
+				- (('INSERT INTO','table') , ('VALUES',(('everything','nothing'),('site','example.com'))))
+			- or a combination of both:
+				- (('INSERT INTO','table') , ('VALUES',{'everything':'nothing','site':'example.com' } ))
+				- {'INSERT INTO':'table' , 'VALUES': (('everything','nothing'),('site','example.com' )) }
+		- Text input using the text positional argument:
+			- a.sql(text='SELECT (*)')
+	- Strings can be built iteratively, or in one go.
+	- a.query_get() returns the string for assignment without popping the query.
+	- a.query_pop() pops the string for assignment.
+	- a.delete(base_string=None) calls __init__.
+	---------------------------------------
+	Usage
+	---------------------------------------
+	>>> a = SQLBuilder()
+	>>> a.sql( (('INSERT INTO','table'),) )
+	'INSERT INTO table '
+	>>> a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )
+	'INSERT INTO table (everything, site) VALUES (nothing, example.com) '
+	>>> a.forget('SELECT (*)')
+	'SELECT (*) '
+	>>> a.sql( [['FROM','table_name']] )
+	'SELECT (*) FROM table_name '
+	>>> a.sql( {'WHERE':(('everything','nothing'),('site','example.com'))} )
+	'SELECT (*) FROM table_name WHERE everything = nothing, site = example.com '
+	>>> a.sql( (('INSERT INTO','table'),) )
+	'INSERT INTO table '
+	>>> a.sql( {'VALUES':{'everything':'nothing','site':'example.com'}} )
+	'INSERT INTO table (everything, site) VALUES (nothing, example.com) '
+	>>> print(a.query_pop())
 	''
-	>>> foo.sql(INSERT_INTO= 'site_visits', VALUES = (('visit_id',1), ('site_url', 'http://example.com')))
-	>>> foo.query()
-	'INSERT INTO site_visits (visit_id, site_url) VALUES (1, http://example.com)'
-	>>> foo.forget('bar:')
-	True
-	>>> foo.query()
-	'bar:'
-	>>> foo.sql(text = 'SELECT "*"')
-	>>> foo.sql(FROM = 'site_visits')
-	>>> foo.sql(VALUES = {'visit_id' : 1 ,  'site_url' : 'http://example.com'})
-	>>> foo.query()
-	'bar: SELECT "*" FROM site_visits (visit_id, site_url) VALUES (1, http://example.com)'
-	>>> foo.forget()
-	True
-	>>> foo.query()
-	''
 	---------------------------------------
-	SQLStatement.sql('starting string') is used to initialise the query string.
-	Takes either:
-	- keyword argument parameters
-		- argument names as the SQL commands, argument values as query values.
-		- for any commands with a space between them (e.g. INSERT INTO)...
-		- ... run them with an underscore (e.g. INSERT_INTO)
-	- Text input using the text positional argument.
-	Strings can be built iteratively, or in one go.
-	SQLStatement.query() returns the string for assignment.
-	---------------------------------------
-	TODO
-	- Auto-capitalise keyword argument variable names (select > SELECT)
+	TODO - more SQL operations
+	TODO - Class methods -> missing, new (?)
+	TODO - WHERE SQL logic for ANDs and ORs...?
+	TODO DONE - Refactor code: move from **kwargs -> *args
 	"""
+	
 	def __init__(self,base_string=None):
 		""" Initialise the string value """
 		if base_string == None:
-			self.query_string = ''
+			self.query_string = str()
 		else:
 			self.query_string = str(base_string) + ' '
-		self.structure_type_checks = [list,tuple]
-		self.value_type_checks = [float,int,str,bool]
+		self.structure_type_checks = set(i for i in [list,tuple,set])
+		self.value_type_checks = set(i for i in [float,int,str,bool])
 	def __mutate__(self,string_value):
 		""" Mutate the current query string with a new string """
 		self.query_string = self.query_string + '%s ' % string_value
 		return True
 	def __values_generator__(self,values,index_selection):
-		""" Extract the right data from data arrays for VALUES sql command """
-		for key,value in values:
-			output = (key,value)
+		""" Extract the right data from data types for VALUES sql command """
+		if type(values) is dict:
+			iterable = values.items()
+		else:
+			iterable = values
+		for k,v in iterable:
+			output = (k,v)
 			if type(output) == str and index_selection != 0:
 				# Not quite working yet
 				yield '"' + '%s' % output[index_selection] + '"'
 			else:
 				yield '%s' % output[index_selection]
-	def __sql_select__(self,key,value):
+	def __sql_select__(self,k,v):
 		""" SQL SELECT LOGIC """
-		self.__mutate__(key)
-		if type(value) in self.structure_type_checks:
-			value_string = ', '.join(map(str,value))
+		self.__mutate__(k)
+		if type(v) in self.structure_type_checks:
+			value_string = ', '.join(map(str,v))
 			self.__mutate__(value_string)
-		elif type(value) in self.value_type_checks:
-			self.__mutate__(value)
-		#else:
-		#	TYPE ERROR
-	def __sql_values__(self,key,value):
+		elif type(v) in self.value_type_checks:
+			self.__mutate__(v)
+		else:
+			TypeError
+	def __sql_values__(self,k,v):
 		""" SQL VALUES LOGIC """
-		if type(value) == dict:
-			name_string, value_string = tuple(', '.join([s for s in self.__values_generator__(value.items(),i)]) for i in range(0,2))
-			self.__mutate__( '(%s)' % name_string + ' %s ' % key + '(%s)' % value_string)
-		elif type(value) in self.structure_type_checks:
-			name_string, value_string = tuple(', '.join([s for s in self.__values_generator__(value,i)]) for i in range(2))
-			self.__mutate__('(%s)' % name_string + ' %s ' % key + '(%s)' % value_string)
-		elif type(value) in self.value_type_checks:
-			self.__mutate__(value)
-		#else:
-		#	TYPE ERROR
-	def __sql_where__(self,key,value):
-		""" SQL WHERE LOGIC - TODO INCOMPLETE!"""
-		self.__mutate__(key)
-		if type(value) == dict:
-			value_string = ', '.join([str(k) + ' = ' + str(v) for k,v in value.items()])
-			self.__mutate__( value_string )
-		elif type(value) in self.structure_type_checks:
-			value_string = ', '.join([str(k) + ' = ' + str(v) for k,v in value])
-			self.__mutate__( value_string )
-		elif type(value) in self.value_type_checks:
-			self.__mutate__(value)
-		#else:
-		#	TYPE ERROR
-	def query(self,string_value=None):
-		""" Return the query, slicing off extra whitespace 
-		- must be sliced to allow for further text additions
+		if type(v) in self.value_type_checks:
+			self.__mutate__(v)
+		elif type(v) in self.structure_type_checks or type(v) is dict:
+			name_string, value_string = tuple(', '.join([s for s in self.__values_generator__(v,i)]) for i in range(2))
+			self.__mutate__('(%s)' % name_string + ' %s ' % k + '(%s)' % value_string)
+		else:
+			TypeError
+	def __sql_where__(self,k,v):
+		""" SQL WHERE LOGIC
+		TODO - how to handle ANDS/ORS ?
 		"""
-		return self.query_string[0:len(self.query_string) - 1 ]
+		self.__mutate__(k)
+		if type(v) in self.value_type_checks:
+			self.__mutate__(v)
+		elif type(v) in self.structure_type_checks or type(v) is dict:
+			value_string = ', '.join([str(x) + ' = ' + str(y) for x,y in v])
+			self.__mutate__( value_string )
+		else:
+			TypeError
+	def query_get(self):
+		""" Return the query (slicing off extra whitespace (is it needed?!?!))
+		- slicing chosen rather than stripping to allow further query clauses to be added
+		"""
+		return self.query_string #[0:len(self.query_string) - 1 ]
+	def query_pop(self):
+		""" Return the query then 'pop' (forget) the value 
+		TODO - this is hacky!
+		"""
+		tmp = self.query_get()
+		self.forget()
+		return tmp
 	def forget(self,base_string=None):
 		""" Start over with a new string """
-		if base_string == None:
-			self.query_string = ''
-		else:
-			self.query_string = str(base_string) + ' '
+		self.__init__(base_string)
+		#if base_string == None:
+		#	self.query_string = ''
+		#else:
+		#	self.query_string = str(base_string) + ' '
 		return True
-	def sql(self,text=None,**kwargs):
+	def sql(self,*args,text=None):
 		""" Run input data through checks and perform necessary actions """
 		if text is not None:
 			if type(text) in self.value_type_checks:
 				self.__mutate__(text)
-			#else:
-			#	TYPE ERROR
-		else:
-			for key, value in kwargs.items():
-				key = key.upper()
+			else:
+				TypeError
+		elif args:
+			args = args[0] # *args puts everything into a single tuple element i.e. (args,)
+			if type(args) in self.structure_type_checks:
+				iterable = args
+			elif type(args) is dict:
+				iterable = args.items()
+			else:
+				TypeError
+			for k, value in iterable:
+				key = k.upper()
 				if '_' in key:
-					key = key.split('_')[0] + ' ' + key.split('_')[1]
+					key = '%s %s' % tuple('hello_world'.split('_'))
 				if key == 'SELECT':
 					self.__sql_select__(key,value)
 				elif key == 'VALUES':
