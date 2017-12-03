7d9dca6dca8657bcc6d4797aa28101e78610a192
Mike <dijksterhuis@users.noreply.github.com>
2017-12-03 03:28:39 +0000

    TypeError capturing

--- SQLStatement.py
+++ SQLStatement.py
@@ -6,13 +6,14 @@ class SQLBuilder:
     
     def __init__(self,base_string=None):
         """ Initialise the string value """
-        if base_string == None:
-            self.query_string = str()
-        else:
-            self.query_string = str(base_string) + ' '
         self.structure_type_checks = set(i for i in [list,tuple,set])
         self.value_type_checks = set(i for i in [float,int,str,bool])
         self.function_list = { 'SELECT' : self.__SELECT__ , 'VALUES' : self.__VALUES__ , 'WHERE' : self.__WHERE__ , 'AND' : self.__WHERE__ , 'OR' : self.__WHERE__ , 'NOT' : self.__WHERE__ }
+        if base_string is None:
+            self.query_string = str()
+        elif type(base_string) in self.value_type_checks:
+            self.query_string = str(base_string) + ' '
+        else: raise TypeError("Unaccepted data type passed to SQLBuilder class. \'base_string\' must be type str")
     
     def __mutate__(self,string_value):
         """ Mutate the current query string with a new string """
@@ -29,7 +30,7 @@ class SQLBuilder:
             output = (k,v)
             if type(output) == str and index_selection != 0:
                 # Not quite working yet
-                yield '"' + '%s' % output[index_selection] + '"'
+                yield '\"' + '%s' % output[index_selection] + '\"'
             else:
                 yield '%s' % output[index_selection]
     
@@ -39,9 +40,8 @@ class SQLBuilder:
             self.__mutate__(v)
         elif type(v) in self.structure_type_checks or type(v) is dict:
             self.function_list[k](k,v)
-        else:
-            TypeError
-    
+        else: raise TypeError("Unaccepted data type passed to .__sql_clause_conditional_logic__ function.")
+            
     def __SELECT__(self,k,v):
         """ SQL SELECT LOGIC """
         self.__mutate__(k)
@@ -77,28 +77,21 @@ class SQLBuilder:
     
     def forget(self,base_string=None):
         """ Start over with a new string """
-        self.__init__(base_string)
-        #if base_string == None:
-        #    self.query_string = ''
-        #else:
-        #    self.query_string = str(base_string) + ' '
-        return True
+        if type(base_string) is not str: raise TypeError("Unaccepted data type passed to forget function. \'base_string\' must be type str")
+        else:
+            self.__init__(base_string)
+            return True
     
     def sql(self,*args,text=None):
         """ Run input data through checks and perform necessary actions """
         if text is not None:
-            if type(text) in self.value_type_checks:
-                self.__mutate__(text)
-            else:
-                TypeError
+            if type(text) is str: self.__mutate__(text)
+            else: raise TypeError("Unaccepted data type passed to .sql function. \'text\' must be type str")
         elif args:
             args = args[0] # *args puts everything into a single tuple element i.e. (args,)
-            if type(args) in self.structure_type_checks:
-                iterable = args
-            elif type(args) is dict:
-                iterable = args.items()
-            else:
-                TypeError
+            if type(args) in self.structure_type_checks: iterable = args
+            elif type(args) is dict: iterable = args.items()
+            else: raise TypeError("Unaccepted data type passed to .sql function. \'args\' must be a mutable sequence.")
             for k, value in iterable:
                 key = k.upper()
                 if '_' in key:
