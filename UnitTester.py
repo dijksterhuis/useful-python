fd7cb2757263aab6a49c90f24a87a20ddcc3547d
Mike <dijksterhuis@users.noreply.github.com>
2017-12-03 02:12:20 +0000

    Docstring update and minor code additions

--- UnitTester.py
+++ UnitTester.py
@@ -1,12 +1,11 @@
 class UnitTester:
+    """ Object to collect results of unit tests (boolean only for now) and then print() results to stdout """
     def __init__(self):
         self.test_results = []
     def add_result(self,result):
-        if type(result) != bool:
-            TypeError
-        else:
-            self.test_results.append(result)
-    def output(self,reset=False):
+        if type(result) != bool: TypeError
+        else: self.test_results.append(result)
+    def output(self,reset=False,indent=1):
         if reset:
             if type(reset) is not bool: TypeError
         if len(self.test_results) < 1:
@@ -14,7 +13,7 @@ class UnitTester:
         elif self.test_results.count(True) != len(self.test_results):
             print('\n{} of {} tests successful'.format(self.test_results.count(True),len(self.test_results)))
             print('\nSome tests failed, here are the results:\n')
-            out_string = ['    test {} status: {}'.format(idx+1,'OK' if value is True else 'FAIL') for idx,value in enumerate(self.test_results)]
+            out_string = [' '*4*indent + 'test {} status: {}'.format(idx+1,'OK' if value is True else 'FAIL') for idx,value in enumerate(self.test_results)]
             print('\n'.join(out_string) + '\n')
         else:
             print('\nAll of {} tests successful\n'.format(len(self.test_results)))
